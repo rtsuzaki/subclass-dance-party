62443a35483a985af8ceebfbc35a23b6eb2c917b
rtsuzaki <rtsuzaki10@gmail.com>
2018-05-01 18:45:16 -0700

    blinkDancer.js is working properly

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -5,7 +5,7 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
- $(this.$node).addClass('blinky')
+ $(this.$node).addClass('klay')
   // return blinkyDancer;
 };
 
